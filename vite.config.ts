a5e900be9bd4d92a39a63ce1fb0190c4065551e9
mintsweet <0x1304570@gmail.com>
2022-11-22 21:03:43 +0800

    feat: optimize build scripts and output content

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
+import { resolve } from 'path';
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
+import { peerDependencies } from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'miller-columns-select',
+      fileName: 'miller-columns-select',
+    },
+    rollupOptions: {
+      external: Object.keys(peerDependencies),
+    },
+  },
 });
