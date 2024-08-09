809126614b9330528e1f71d53220b3da6a8c7c18
mintsweet <0x1304570@gmail.com>
2024-08-09 16:46:47 +1200

    refactor: use miller-column to replace old index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-import type { IMillerColumns } from './types';
-import { checkId } from './utils';
-
-export const MillerColumns = <T>({ items }: IMillerColumns<T>) => {
-  if (!checkId(items)) {
-    throw new Error('Items must have unique id and parentId');
-  }
-
-  return null;
-};
+export * from './miller-columns';
