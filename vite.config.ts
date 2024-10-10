0e13bbe064107391d48bf1f800b9e6d4723d0d15
mintsweet <0x1304570@gmail.com>
2024-10-10 20:47:05 +1300

    refactor: use vite-plugin-dts to generate single type file

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { resolve } from 'path';
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 import { dependencies, peerDependencies } from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ outDir: 'types', rollupTypes: true })],
 
   build: {
     lib: {
